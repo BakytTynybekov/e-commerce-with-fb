9aa4033e7aefb8c9833eb43fcc9d9ca6f9ef0b5b
Bakyt Tynybekov <bakyt.tynybekov.ss@gmail.com>
2022-04-16 15:47:30 +0300

    added file to gitignore

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,15 +11,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBP_scgKu-ToweYu2s07TD-TTfLATMEUsA",
-//   authDomain: "reactfirabase-79889.firebaseapp.com",
-//   projectId: "reactfirabase-79889",
-//   storageBucket: "reactfirabase-79889.appspot.com",
-//   messagingSenderId: "322660685354",
-//   appId: "1:322660685354:web:20b4a99c61fdfeadfad553",
-// };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const fireStore = getFirestore(app);
